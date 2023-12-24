507bd6d933f53d5697aa3b887fa0676eb79b7947
Quentin Langlois <quentin.langlois147@gmail.com>
2023-12-24 18:25:34 +0100

    home fini

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,10 @@ const Home = () => {
   return (
     <div className="home">
       <div className="home-content">
-        <div className="role">Votre rôle professionnel ici</div>
+        <div className=''>
+          <div className="role">Développeur web passionné</div>
+          <div className="description">Bienvenue dans mon monde, où créativité et technologie fusionnent. Expert en solutions web interactives, allant de React à une gamme étendue d'autres technologies, je mets en œuvre mon expertise pour transformer vos idées en expériences utilisateur captivantes et fonctionnelles. Découvrez comment ma polyvalence et mon approche créative peuvent enrichir vos projets.</div>
+        </div>
         <div className="image-container">
           <img src="pfp.png" alt="Illustration Personnalisée" />
         </div>
